b617d9f22ceb777b9022bc4aa96de8a3c9a1f0bf
leotok <leotok@hotmail.com>
2015-09-30 16:11:22 -0300

    mapmenu drag

--- FinalChallenge/MapMenu.swift
+++ FinalChallenge/MapMenu.swift
@@ -12,21 +12,23 @@ import SpriteKit
 class MapMenu: SKScene, UIGestureRecognizerDelegate {
     
     var parentScene: SKScene!
+    var background: SKSpriteNode!
     
     override func didMoveToView(view: SKView) {
         
-        let background = SKSpriteNode(imageNamed: "desert")
-        background.position = CGPointMake( self.size.width / 2 , self.size.height / 2 )
-        background.size = self.size
+        background = SKSpriteNode(imageNamed: "desert")
+        background.anchorPoint = CGPointZero
+        background.position = CGPointZero
+        background.size = CGSizeMake(self.size.width * 1.5, self.size.height * 1.5)
         background.zPosition = -1
         
         addChild(background)
         
         // PanGesture
         
-//        var panGesture = UIPanGestureRecognizer(target: self, action: Selector("dragMap"))
-//        panGesture.delegate = self
-//        self.view?.addGestureRecognizer(panGesture)
+        let panGesture = UIPanGestureRecognizer(target: self, action: Selector("dragMap:"))
+        panGesture.delegate = self
+        self.view?.addGestureRecognizer(panGesture)
         
         // Back Button
         
@@ -66,24 +68,22 @@ class MapMenu: SKScene, UIGestureRecognizerDelegate {
         }
     }
     
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    func dragMap(gesture: UIPanGestureRecognizer) {
         
-        for touch in (touches) {
-            let location = touch.locationInNode(self)
-            let node = self.nodeAtPoint(location)
-            
-            print("touch Moved")
-            
-            if node.zPosition == -1 {
-                
-                print("touched background")
-                if let gesture = touch.gestureRecognizers?.first {
-                    
-                }
-                
-            }
-            
+        let translation = gesture.translationInView(self.view)
+        
+        var newXPosition: CGFloat = background.position.x + translation.x
+        
+        if translation.x > 0 {
+            newXPosition = min(newXPosition , 0)
         }
+        else if translation.x < 0 {
+            newXPosition = max(newXPosition , self.size.width - background.size.width)
+        }
+        
+        background.position.x = newXPosition
+        
+        gesture.setTranslation(CGPointZero, inView: self.view)
     }
     
     func addSamLevels() {
@@ -93,42 +93,42 @@ class MapMenu: SKScene, UIGestureRecognizerDelegate {
         level1.size = CGSize(width: 40, height: 40)
         level1.name = "1"
         
-        addChild(level1)
+        background.addChild(level1)
         
         let level2 = SKSpriteNode(imageNamed: "clock")
         level2.position = CGPointMake(90, 190)
         level2.size = CGSize(width: 40, height: 40)
         level2.name = "2"
         
-        addChild(level2)
+        background.addChild(level2)
         
         let level3 = SKSpriteNode(imageNamed: "clock")
         level3.position = CGPointMake(150, 150)
         level3.size = CGSize(width: 40, height: 40)
         level3.name = "3"
         
-        addChild(level3)
+        background.addChild(level3)
         
         let level4 = SKSpriteNode(imageNamed: "clock")
         level4.position = CGPointMake(210, 110)
         level4.size = CGSize(width: 40, height: 40)
         level4.name = "4"
         
-        addChild(level4)
+        background.addChild(level4)
         
         let level5 = SKSpriteNode(imageNamed: "clock")
         level5.position = CGPointMake(270, 140)
         level5.size = CGSize(width: 40, height: 40)
         level5.name = "5"
         
-        addChild(level5)
+        background.addChild(level5)
         
         let level6 = SKSpriteNode(imageNamed: "clock")
         level6.position = CGPointMake(320, 200)
         level6.size = CGSize(width: 40, height: 40)
         level6.name = "6"
         
-        addChild(level6)
+        background.addChild(level6)
     }
     
     func addShrinkLevels() {
@@ -138,42 +138,42 @@ class MapMenu: SKScene, UIGestureRecognizerDelegate {
         level7.size = CGSize(width: 40, height: 40)
         level7.name = "7"
         
-        addChild(level7)
+        background.addChild(level7)
         
         let level8 = SKSpriteNode(imageNamed: "freud")
         level8.position = CGPointMake(420, 250)
         level8.size = CGSize(width: 40, height: 40)
         level8.name = "8"
         
-        addChild(level8)
+        background.addChild(level8)
         
         let level9 = SKSpriteNode(imageNamed: "freud")
         level9.position = CGPointMake(470, 270)
         level9.size = CGSize(width: 40, height: 40)
         level9.name = "9"
         
-        addChild(level9)
+        background.addChild(level9)
         
         let level10 = SKSpriteNode(imageNamed: "freud")
         level10.position = CGPointMake(520, 300)
         level10.size = CGSize(width: 40, height: 40)
         level10.name = "10"
         
-        addChild(level10)
+        background.addChild(level10)
         
         let level11 = SKSpriteNode(imageNamed: "freud")
         level11.position = CGPointMake(570, 270)
         level11.size = CGSize(width: 40, height: 40)
         level11.name = "11"
         
-        addChild(level11)
+        background.addChild(level11)
         
         let level12 = SKSpriteNode(imageNamed: "freud")
         level12.position = CGPointMake(620, 230)
         level12.size = CGSize(width: 40, height: 40)
         level12.name = "12"
         
-        addChild(level12)
+        background.addChild(level12)
     }
     
     func addEllieLevels() {
@@ -184,42 +184,42 @@ class MapMenu: SKScene, UIGestureRecognizerDelegate {
         level13.size = CGSize(width: 40, height: 40)
         level13.name = "13"
         
-        addChild(level13)
+        background.addChild(level13)
         
         let level14 = SKSpriteNode(imageNamed: "fat-bottom-girls")
         level14.position = CGPointMake(420, 170)
         level14.size = CGSize(width: 40, height: 40)
         level14.name = "14"
         
-        addChild(level14)
+        background.addChild(level14)
         
         let level15 = SKSpriteNode(imageNamed: "fat-bottom-girls")
         level15.position = CGPointMake(470, 150)
         level15.size = CGSize(width: 40, height: 40)
         level15.name = "15"
         
-        addChild(level15)
+        background.addChild(level15)
         
         let level16 = SKSpriteNode(imageNamed: "fat-bottom-girls")
         level16.position = CGPointMake(520, 130)
         level16.size = CGSize(width: 40, height: 40)
         level16.name = "16"
         
-        addChild(level16)
+        background.addChild(level16)
         
         let level17 = SKSpriteNode(imageNamed: "fat-bottom-girls")
         level17.position = CGPointMake(570, 120)
         level17.size = CGSize(width: 40, height: 40)
         level17.name = "17"
         
-        addChild(level17)
+        background.addChild(level17)
         
         let level18 = SKSpriteNode(imageNamed: "fat-bottom-girls")
         level18.position = CGPointMake(620, 140)
         level18.size = CGSize(width: 40, height: 40)
         level18.name = "18"
         
-        addChild(level18)
+        background.addChild(level18)
         
     }
 }

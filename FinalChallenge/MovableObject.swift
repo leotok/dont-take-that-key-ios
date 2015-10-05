680824105b4c9ac6935ab1528d5845763831302d
leotok <leotok@hotmail.com>
2015-10-05 16:42:06 -0300

    new crate

--- FinalChallenge/MovableObject.swift
+++ FinalChallenge/MovableObject.swift
@@ -11,4 +11,22 @@ import SpriteKit
 
 class MovableObject: GenericObject {
     
+    override init(sprite: SKTexture) {
+        
+        super.init(sprite: sprite)
+        zPosition = 10
+        physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(32, 32))
+        physicsBody?.categoryBitMask = 2
+        physicsBody?.collisionBitMask = 1 | 2
+        physicsBody?.affectedByGravity = true
+        physicsBody?.allContactedBodies()
+        physicsBody?.allowsRotation = false
+        physicsBody?.restitution = 0
+        physicsBody?.dynamic = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+
+    }
 }
